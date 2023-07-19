3199b92d02769d61b3169eed7da5b331762018f4
Krzys3n <krzys3n@mgail.com>
2023-07-19 15:04:23 +0200

    edit profile, acces to apps, view of info

--- AppFixB1/forms.py
+++ AppFixB1/forms.py
@@ -2,11 +2,69 @@ from django import forms
 from .models import App,Report
 
 
+from django import forms
+from .models import App
+
+from django import forms
+from .models import App
+
+from django import forms
+from .models import App
+
+from django import forms
+from .models import App
+
 class AppForm(forms.ModelForm):
+    CATEGORY_CHOICES = [
+        ('Desktop App', 'Desktop App'),
+        ('Web App', 'Web App'),
+        ('Game', 'Game'),
+        ('Mobile App', 'Mobile App'),
+        ('other', 'Other'),
+    ]
+
+    category = forms.ChoiceField(choices=CATEGORY_CHOICES)
+    custom_category = forms.CharField(required=False, label = "" )
+
     class Meta:
         model = App
-        fields = ['name', 'description']
-        labels = {'description': 'Opis Aplikacji', 'name': 'Nazwa Aplikacji'}
+        fields = ['name', 'description', 'category', 'custom_category', 'price', 'version', 'access']
+        labels = {
+            'name': 'App Name',
+            'description': 'App Description',
+            'category': 'Category:',
+            'custom_category': '',
+            'price': 'Price',
+            'version': 'Version',
+            'access': 'Acces',
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['category'].widget.attrs['class'] = 'inline-select'
+        self.fields['custom_category'].widget.attrs['class'] = 'inline-input'
+        self.fields['custom_category'].widget.attrs['placeholder'] = 'Input your own app category'
+        self.fields['custom_category'].widget.attrs['style'] = 'display:none;'
+        self.fields['category'].widget.attrs['onchange'] = 'toggleCustomCategory()'
+
+    def clean(self):
+        cleaned_data = super().clean()
+        category = cleaned_data.get('category')
+        custom_category = cleaned_data.get('custom_category')
+        if category == 'other':
+            self.fields['custom_category'].widget.attrs['style'] = 'display:true;'
+        if category == 'other' and custom_category:
+
+            category = custom_category  # Użyj wprowadzonej wartości jako kategorii
+
+        cleaned_data['category'] = category  # Zapisz zaktualizowaną wartość kategorii
+
+        if category == 'other' and not custom_category:
+            raise forms.ValidationError('Please enter your own category name.')
+
+        return cleaned_data
+
+
 
 
 class ReportForm(forms.ModelForm):
--- AppFixB1/views.py
+++ AppFixB1/views.py
@@ -20,18 +20,19 @@ def index(request):
 def apps(request):
     # """Wyświetlenie wszystkich tematów."""
 
-    apps = App.objects.filter(owner=request.user).order_by('date_added')
+    apps = App.objects.order_by('date_added')
+    user = request.user
 
 
     # apps = App.objects.order_by('date_added')
-    context = {'apps': apps}
+    context = {'apps': apps, 'user': user}
     return render(request, 'apps.html', context)
 
 @login_required
 def app(request, app_id):
     app = App.objects.get(id=app_id)
 
-    check_app_owner(request,app)
+
 
     reports = app.report_set.order_by('-date_created')
     context = {'app': app, 'reports': reports}
@@ -103,7 +104,39 @@ def view_profile(request, user_id):
     context = {'user' : user}
     return render(request,'view_profile.html', context)
 
-def edit_profile(request, user_id):
-    return render(request,'edit_profile.html')
+@login_required
+def edit_profile(request):
+    user = request.user
+
+    if request.method == 'POST':
+
+        discord_profile = request.POST.get('discord_profile')
+        github_profile = request.POST.get('github_profile')
+        linkedin_profile = request.POST.get('linkedin_profile')
+        twitter_profile = request.POST.get('twitter_profile')
+        phone_number = request.POST.get('phone_number')
+        email = request.POST.get('email')
+
+
+        user.discord_profile = discord_profile
+        user.github_profile = github_profile
+        user.linkedin_profile = linkedin_profile
+        user.twitter_profile = twitter_profile
+        user.phone_number = phone_number
+        user.email = email
+        user.save()
+
+        return redirect('view_profile', user.id)
+    else:
+        initial_data = {
+
+            'discord_profile': user.discord_profile,
+            'github_profile': user.github_profile,
+            'linkedin_profile': user.linkedin_profile,
+            'twitter_profile': user.twitter_profile,
+            'phone_number': user.phone_number,
+            'email': user.email
+        }
+        return render(request, 'edit_profile.html', {'initial_data': initial_data})
 
 
